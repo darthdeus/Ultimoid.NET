6b090bc081dc96e24876c3155afe5a0ac74604b1
Jakub Arnold <darthdeus@gmail.com>
2017-10-06 16:04:52 +0200

    Add basic network manager

--- Ultimoid.Lib/Protocol.cs
+++ Ultimoid.Lib/Protocol.cs
@@ -1,100 +1,160 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Collections.Generic;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
 
 namespace Ultimoid.Lib {
-    public struct Datagram {
-        public UInt64 Seq;
-        public UInt32 Ack;
-        public byte[] Payload;
-    }
+	public struct Datagram {
+		public UInt64 Seq;
+		public UInt64 Ack;
+		public UInt32 AckField;
+		public byte[] Payload;
+	}
 
-    public static class Protocol {
-        public const int MagicHeaderLength = 5;
+	public class NetworkManager {
+		const int ListenPort = 9987;
 
-        private static readonly byte[] MagicBytes = new byte[] {0xCA, 0xFF, 0xEE, 0xFF, 0xAC};
+		private Scheduler _scheduler;
+		private Dictionary<ulong, CancellationToken> _pending = new Dictionary<ulong, CancellationToken>();
+		private UdpClient _udp;
 
-        const int SeqLength = sizeof(UInt64);
-        const int AckLength = sizeof(UInt32);
-        const int HeaderLength = MagicHeaderLength + SeqLength + AckLength;
-        const int MaxDatagramLength = 256;
+		public NetworkManager(Scheduler scheduler) {
+			_scheduler = scheduler;
 
-        public static Datagram Deserialize(byte[] networkData) {
-            if (networkData.Length > MaxDatagramLength) {
-                throw new ArgumentException($"Datagram longer than maximum allowed size (actual size: ${networkData.Length}",
-                    nameof(networkData));
-            }
+			// TODO: listen port
+			_udp = new UdpClient(ListenPort);
+		}
 
-            if (!IsMagicNumberValid(networkData)) {
-                throw new ArgumentException($"Attempting to deserialize a datagram in an INVALID format",
-                    nameof(networkData));
-            }
+		public void ReliableSend(Datagram datagram) {
+			// TODO: vymyslet generovani seq
+			DoSend(datagram);
 
-            if (networkData.Length - HeaderLength < 0) {
-                throw new ArgumentException(
-                    $"Network data is shorter than header size (size: ${networkData.Length}, header size: {HeaderLength}",
-                    nameof(networkData));
-            }
+			// TODO: compute based on initial handshake?
+			TimeSpan retryPeriod = TimeSpan.FromMilliseconds(200);
+			int retryCount = 3;
 
-            Datagram result;
+			var tcs = _scheduler.RunPeriodicallyLimited(retryPeriod, retryCount, () => {
+				// TODO: compute seq
+				DoSend(datagram);
+			});
+		}
 
-            byte[] seqBytes = new byte[SeqLength];
-            Array.Copy(networkData, MagicHeaderLength, seqBytes, 0, seqBytes.Length);
-            seqBytes.ReverseIfLittleEndian();
+		private void DoSend(Datagram datagram) {
+			// TODO: samotne odeslani datagramu
+		}
 
-            result.Seq = BitConverter.ToUInt64(seqBytes, 0);
 
-            byte[] ackBytes = new byte[AckLength];
-            Array.Copy(networkData, MagicHeaderLength + SeqLength, ackBytes, 0, ackBytes.Length);
-            ackBytes.ReverseIfLittleEndian();
 
-            result.Ack = BitConverter.ToUInt32(ackBytes, 0);
+		public void Update(TimeSpan deltaTime) {
+			IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
 
-            byte[] payload = new byte[networkData.Length - HeaderLength];
-            Array.Copy(networkData, HeaderLength, payload, 0, payload.Length);
 
-            result.Payload = payload;
+			byte[] payload = _udp.Receive(ref sender);
 
-            return result;
-        }
 
-        public static byte[] Serialize(Datagram datagram) {
-            return Serialize(datagram.Payload, datagram.Seq, datagram.Ack);
-        }
+		}
+	}
 
-        public static byte[] Serialize(byte[] payload, UInt64 seq, UInt32 ack) {
-            var stream = new MemoryStream();
+	public static class Protocol {
+		public const int MagicHeaderLength = 5;
 
-            // Magic header
-            stream.Write(MagicBytes, 0, MagicBytes.Length);
+		private static readonly byte[] MagicBytes = new byte[] { 0xCA, 0xFF, 0xEE, 0xFF, 0xAC };
 
-            Debug.Assert(stream.Length == MagicHeaderLength);
+		const int SeqLength = sizeof(UInt64);
+		const int AckLength = sizeof(UInt64);
+		const int AckFieldLength = sizeof(UInt32);
+		const int HeaderLength = MagicHeaderLength + SeqLength + AckLength + AckFieldLength;
+		const int MaxDatagramLength = 256;
 
-            byte[] seqBytes = BitConverter.GetBytes(seq);
-            seqBytes.ReverseIfLittleEndian();
-            stream.Write(seqBytes, 0, seqBytes.Length);
+		public static Datagram Deserialize(byte[] networkData) {
+			if (networkData.Length > MaxDatagramLength) {
+				throw new ArgumentException($"Datagram longer than maximum allowed size (actual size: ${networkData.Length}",
+					nameof(networkData));
+			}
 
-            byte[] ackBytes = BitConverter.GetBytes(ack);
-            ackBytes.ReverseIfLittleEndian();
-            stream.Write(ackBytes, 0, ackBytes.Length);
+			if (!IsMagicNumberValid(networkData)) {
+				throw new ArgumentException($"Attempting to deserialize a datagram in an INVALID format",
+					nameof(networkData));
+			}
 
-            stream.Write(payload, 0, payload.Length);
+			if (networkData.Length - HeaderLength < 0) {
+				throw new ArgumentException(
+					$"Network data is shorter than header size (size: ${networkData.Length}, header size: {HeaderLength}",
+					nameof(networkData));
+			}
 
-            return stream.ToArray();
-        }
+			Datagram result;
 
-        public static bool IsMagicNumberValid(byte[] data) {
-            return data[0] == MagicBytes[0] &&
-                   data[1] == MagicBytes[1] &&
-                   data[2] == MagicBytes[2] &&
-                   data[3] == MagicBytes[3] &&
-                   data[4] == MagicBytes[4];
-        }
+			byte[] seqBytes = new byte[SeqLength];
+			Array.Copy(networkData, MagicHeaderLength, seqBytes, 0, seqBytes.Length);
+			seqBytes.ReverseIfLittleEndian();
 
-        public static void ReverseIfLittleEndian(this Array arr) {
-            if (BitConverter.IsLittleEndian) {
-                Array.Reverse(arr);
-            }
-        }
-    }
+			result.Seq = BitConverter.ToUInt64(seqBytes, 0);
+
+			byte[] ackBytes = new byte[AckLength];
+			Array.Copy(networkData, MagicHeaderLength + SeqLength, ackBytes, 0, ackBytes.Length);
+			ackBytes.ReverseIfLittleEndian();
+
+			result.Ack = BitConverter.ToUInt64(ackBytes, 0);
+
+			byte[] ackFieldBytes = new byte[AckFieldLength];
+			Array.Copy(networkData, MagicHeaderLength + SeqLength + AckLength, ackFieldBytes, 0, ackFieldBytes.Length);
+			ackFieldBytes.ReverseIfLittleEndian();
+
+			result.AckField = BitConverter.ToUInt32(ackFieldBytes, 0);
+
+			byte[] payload = new byte[networkData.Length - HeaderLength];
+			Array.Copy(networkData, HeaderLength, payload, 0, payload.Length);
+
+			result.Payload = payload;
+
+			return result;
+		}
+
+		public static byte[] Serialize(Datagram datagram) {
+			return Serialize(datagram.Payload, datagram.Seq, datagram.Ack, datagram.AckField);
+		}
+
+		public static byte[] Serialize(byte[] payload, UInt64 seq, UInt64 ack, UInt32 ackField) {
+			var stream = new MemoryStream();
+
+			// Magic header
+			stream.Write(MagicBytes, 0, MagicBytes.Length);
+
+			Debug.Assert(stream.Length == MagicHeaderLength);
+
+			byte[] seqBytes = BitConverter.GetBytes(seq);
+			seqBytes.ReverseIfLittleEndian();
+			stream.Write(seqBytes, 0, seqBytes.Length);
+
+			byte[] ackBytes = BitConverter.GetBytes(ack);
+			ackBytes.ReverseIfLittleEndian();
+			stream.Write(ackBytes, 0, ackBytes.Length);
+
+			byte[] ackFieldBytes = BitConverter.GetBytes(ackField);
+			ackFieldBytes.ReverseIfLittleEndian();
+			stream.Write(ackFieldBytes, 0, ackFieldBytes.Length);
+
+			stream.Write(payload, 0, payload.Length);
+
+			return stream.ToArray();
+		}
+
+		public static bool IsMagicNumberValid(byte[] data) {
+			return data[0] == MagicBytes[0] &&
+				   data[1] == MagicBytes[1] &&
+				   data[2] == MagicBytes[2] &&
+				   data[3] == MagicBytes[3] &&
+				   data[4] == MagicBytes[4];
+		}
+
+		public static void ReverseIfLittleEndian(this Array arr) {
+			if (BitConverter.IsLittleEndian) {
+				Array.Reverse(arr);
+			}
+		}
+	}
 }
--- Ultimoid.Server/Program.cs
+++ Ultimoid.Server/Program.cs
@@ -8,17 +8,33 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Ultimoid.Server {
-    class Program {
-        static void Main(string[] args) {
-            var client = new UdpClient(new IPEndPoint(IPAddress.Any, 8989));
+	class Program {
+		static void RunServerLoop(int port) {
+			var udp = new UdpClient(port);
+			udp.DontFragment = true;
 
-            while (true) {
-                IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+			while (true) {
+				IPEndPoint remote = new IPEndPoint(IPAddress.Any, 0);
 
-                var data = client.Receive(ref sender);
-                Console.WriteLine("Received:");
-                Console.WriteLine(Encoding.ASCII.GetString(data));
-            }
-        }
-    }
+				Console.WriteLine("Waiting");
+				byte[] payload = udp.Receive(ref remote);
+
+				Console.WriteLine($"Received {payload.Length}, IP: {remote}, data: {string.Join(" ", payload.Select(x => x.ToString()))}");
+			}
+		}
+
+		static void Main(string[] args) {
+			RunServerLoop(7999);
+
+			//var client = new UdpClient(new IPEndPoint(IPAddress.Any, 8989));
+
+			//while (true) {
+			//    IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+
+			//    var data = client.Receive(ref sender);
+			//    Console.WriteLine("Received:");
+			//    Console.WriteLine(Encoding.ASCII.GetString(data));
+			//}
+		}
+	}
 }
