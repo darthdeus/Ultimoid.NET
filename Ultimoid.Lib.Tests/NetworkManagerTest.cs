6f806f76c32aeca1953c6adfe848ac893d16c737
Jakub Arnold <darthdeus@gmail.com>
2018-08-13 09:14:25 +0200

    Preparations for mock UDP client

---
+++ Ultimoid.Lib.Tests/NetworkManagerTest.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ultimoid.Lib.Tests {
+    public class FakeUdpClient {
+
+    }
+
+    [TestClass]
+    public class NetworkManagerTest {
+
+    }
+}
---
+++ Ultimoid.Lib/IUdpNetworkClient.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Ultimoid.Lib {
+    public interface IUdpNetworkClient {
+        byte[] Receive(ref IPEndPoint remote);
+        int Send(byte[] data, int length, IPEndPoint remoteEndpoint);
+    }
+
+    public class UdpNetworkClient : IUdpNetworkClient {
+        private readonly UdpClient _client;
+
+        public UdpNetworkClient(UdpClient client) {
+            _client = client;
+        }
+
+        public byte[] Receive(ref IPEndPoint remote) {
+            return _client.Receive(ref remote);
+        }
+
+        public int Send(byte[] data, int length, IPEndPoint remoteEndpoint) {
+            return _client.Send(data, length, remoteEndpoint);
+        }
+    }
+}
